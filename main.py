7aaf82a7d28dc880266050f28b251ed283ae7ac4
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-17 21:59:26 +0200

    added greeting, enhanced exception handling

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from sys import exit
 from num2words import num2words
+import decimal
 
 
 def main() -> None:
+    print('Welcome to The Number-to-Names Converter!')
+    exit_message: str = 'Exiting program...'
     while True:
         try:
-            user_input = input('Ente a number: ')
+            user_input: str = input('Enter a number: ')
 
             if user_input == 'exit':
-                print('Thanks for trying my program!')
+                print(exit_message)
                 exit()
+
             print(num2words(user_input))
 
+        except decimal.InvalidOperation:
+            print('Please enter some valid input...')
+            continue
+
         except KeyboardInterrupt:
-            print('Exiting...')
+            print(exit_message)
             exit()
 
 
